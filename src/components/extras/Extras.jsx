7ef4136c6ba1d17b739c0800096eaa43eb5ba665
Priyankar01 <priyankardutta001@gmail.com>
2024-01-12 23:35:55 +0530

    made design responsive

--- src/components/extras/Extras.jsx
+++ src/components/extras/Extras.jsx
@@ -18,36 +18,42 @@ function Extras() {
 								src="https://www.starbucks.com/weblx/images/rewards/benefits/1-fun-freebies.jpg"
 								alt=""
 							/>
-							<h3>Fun freebies</h3>
-							<p>
-								Not only can you earn free coffee, look forward to a birthday
-								treat plus coffee and tea refills.
-							</p>
-							<a href="">Learn more</a>
+							<div>
+								<h3>Fun freebies</h3>
+								<p>
+									Not only can you earn free coffee, look forward to a birthday
+									treat plus coffee and tea refills.
+								</p>
+								<a href="">Learn more</a>
+							</div>
 						</li>
 						<li>
 							<img
 								src="https://www.starbucks.com/weblx/images/rewards/benefits/2-order-and-pay-ahead.jpg"
 								alt=""
-							/>{' '}
-							<h3>Order & pay ahead</h3>
-							<p>
-								Enjoy the convenience of in-store, curbside or drive-thru pickup
-								at select stores.
-							</p>
-							<a href="">Learn more</a>
+							/>
+							<div>
+								<h3>Order & pay ahead</h3>
+								<p>
+									Enjoy the convenience of in-store, curbside or drive-thru
+									pickup at select stores.
+								</p>
+								<a href="">Learn more</a>
+							</div>
 						</li>
 						<li>
 							<img
 								src="https://www.starbucks.com/weblx/images/rewards/benefits/3-get-to-free-faster.jpg"
 								alt=""
-							/>{' '}
-							<h3>Get to free faster</h3>
-							<p>
-								Earn Stars even quicker with Bonus Star challenges, Double Star
-								Days and exciting games.
-							</p>{' '}
-							<a href="">Learn more</a>
+							/>
+							<div>
+								<h3>Get to free faster</h3>
+								<p>
+									Earn Stars even quicker with Bonus Star challenges, Double
+									Star Days and exciting games.
+								</p>
+								<a href="">Learn more</a>
+							</div>
 						</li>
 					</ul>
 				</div>
